794bb31a2bafe4ba685368c1c176281b7d5ff379
Sofiane <132440690+Sofiane5900@users.noreply.github.com>
2024-12-16 11:32:32 +0100

    ajout du tableau et du for

--- PairOuImpair/Program.cs
+++ PairOuImpair/Program.cs
@@ -6,7 +6,17 @@ namespace PairOuImpair // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("--- Est pair...? Est impair...? ---");
+            Console.Write("Combien de nombres contiendra le tableau ? : ");
+            int userInput = int.Parse(Console.ReadLine());
+            int[] tableau = new int[userInput];
+            Console.WriteLine("Affectation automatique des valeurs...");
+
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                Random random = new Random();
+            }
+
         }
     }
 }
